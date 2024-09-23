4a9fe157759f97d4af183e9af1689ead1399a771
Matej Poklemba <matejpoklemba@gmail.com>
2024-09-23 16:33:29 +0200

    Add root files

---
+++ hardhat.config.ts
@@ -0,0 +1,42 @@
+import type { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+
+const config: HardhatUserConfig = {
+  typechain: {
+    externalArtifacts: ['**/abi/*.json'],
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.24',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: '0.8.12',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+};
+
+// eslint-disable-next-line import/no-default-export
+export default config;
